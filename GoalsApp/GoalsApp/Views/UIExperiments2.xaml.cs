338d49718c54cdb734a63c7ed9b8c21272db8a21
Josh Dellamuth <joshdellamuth@gmail.com>
2023-11-18 01:34:19 -0600

    Defined UIExperiments 2 to define UI in C#

--- GoalsApp/GoalsApp/Views/UIExperiments2.xaml.cs
+++ GoalsApp/GoalsApp/Views/UIExperiments2.xaml.cs
@@ -1,9 +1,24 @@
+using Microsoft.Maui.Controls;
+
 namespace GoalsApp.Views;
 
 public partial class UIExperiments2 : ContentPage
 {
 	public UIExperiments2()
 	{
-		InitializeComponent();
-	}
+        //If all code is done in c#, no need to call initialize component
+        //InitializeComponent();
+
+        var button = new Button { Text = "Click me!" };
+        var label = new Label { Text = "Hello, World!" };
+
+        button.Clicked += (s, e) => {
+            label.Text = "Button clicked!";
+        };
+
+        Content = new StackLayout
+        {
+            Children = { button, label }
+        };
+    }
 }

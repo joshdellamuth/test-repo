94dacd94a7c8d0f228254d85c71a9d8d19cb80d5
Josh Dellamuth <joshdellamuth@gmail.com>
2023-11-19 16:30:28 -0600

    Removed titles at top of pages, Community NuGet Package update, expander class

--- GoalsApp/GoalsApp/Views/UIExperiments2.xaml.cs
+++ GoalsApp/GoalsApp/Views/UIExperiments2.xaml.cs
@@ -1,24 +1,47 @@
 using Microsoft.Maui.Controls;
+using CommunityToolkit.Maui.Views;
+using Microsoft.Maui.Controls.Shapes;
 
 namespace GoalsApp.Views;
 
 public partial class UIExperiments2 : ContentPage
 {
-	public UIExperiments2()
-	{
+    public UIExperiments2()
+    {
         //If all code is done in c#, no need to call initialize component
         //InitializeComponent();
 
-        var button = new Button { Text = "Click me!" };
-        var label = new Label { Text = "Hello, World!" };
+        var label = new Label { Text = "My Tasks" };
 
-        button.Clicked += (s, e) => {
-            label.Text = "Button clicked!";
+        //button.Clicked has our object (button) getting clicked
+        //+= is used to subscribe to a method 
+        //button.Clicked += (s, e) => {
+        //    label.Text = "Button clicked!";
+        //};
+
+        var border = new Border
+        {
+            Content = new Expander
+            {
+                Header = new Label { Text = "Section 1" },
+                Content = new Label { Text = "Content for section 1" }
+            },
+            Padding = new Thickness(10),
+            StrokeShape = new RoundRectangle
+            {
+                CornerRadius = new CornerRadius(10,10,10,10)
+            },
+            Background = new SolidColorBrush(Colors.LightGrey)//,
+            //StrokeThickness = 4,
+
         };
+        
+
 
         Content = new StackLayout
         {
-            Children = { button, label }
+            Padding = new Thickness(10),
+            Children = { label, border }
         };
     }
 }
